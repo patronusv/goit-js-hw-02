c64375ffe517a609b499bd4fcb0c14d9631d6082
Viktor Gorokhov <patronusv@gmail.com>
2020-11-10 01:08:02 +0200

    добавляет решение 4-го задания

--- js/task-4.js
+++ js/task-4.js
@@ -2,14 +2,31 @@
 
 function formatString (string, maxLength = 40) {
   // Write code under this line
-  
+    let array = [];
+    array = string.split('');
+    if (array.length < maxLength) {
+        string = array.join('');
+    } else {
+        for (let i = maxLength; i < string.length; i += 1){
+        array.pop();        
+        }
+        for (let i = 0; i < 3; i += 1){
+            array.push('.');
+        }
+        string = array.join('');
+    }
+
+    console.log(array);
+    console.log(string);
+    return string;
+    
 }
 
-//console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 // 'Curabitur ligula sapien, tincidunt non.'
 
-//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
 // 'Vestibulum facilisis, purus nec pulvinar...'
 
-//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
 // 'Vestibulum facilisis, purus ne...'
