1d6df1d1f5e29c3c9052493cb160d3d61e4a5b75
Viktor Gorokhov <patronusv@gmail.com>
2020-11-09 22:54:31 +0200

    добавляет решение 3-го задания

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+'use strict'
+function findLongestWord (string = "") {
+  // Write code under this line
+    let array
+let result = '';
+    array = string.split(' ');
+    for (let i = 0; i < array.length; i += 1){
+        if (result.length < array[i].length) {
+            result = array[i]
+        }
+    }
+    console.log(array);
+    console.log(result);
+    return result;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
