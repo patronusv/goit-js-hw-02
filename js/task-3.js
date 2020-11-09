51f30ddef33a631a97a20346ba23097f7f790026
Viktor Gorokhov <patronusv@gmail.com>
2020-11-09 23:35:24 +0200

    update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 'use strict'
 function findLongestWord (string = "") {
   // Write code under this line
-    let array;
-let result = '';
+    let array = [];
+    let result = '';
     array = string.split(' ');
     for (let i = 0; i < array.length; i += 1){
         if (result.length < array[i].length) {
---
+++ js/task-4.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function formatString (string, maxLength = 40) {
+  // Write code under this line
+  
+}
+
+//console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+// 'Vestibulum facilisis, purus ne...'
